58fbefcdc09c7595c8c12b68d2377ade54913a95
Konrad Bauer <konradbauer94@gmail.com>
2023-12-17 03:05:04 +0100

    Header styles fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,25 @@
 import bookmark from "../../images/logo-bookmark.svg";
 
-export const Header = () => {
+export const Header: React.FC = () => {
   const navigation = ["features", "pricing", "contact"];
 
   return (
-    <div className="navbar flex justify-between items-center tracking-wider">
-      <div className="navbar-start">
-        <a>
+    <div className="flex flex-col lg:flex-row items-center tracking-wider mx-16 lg:mx-24">
+      <div className="mx-8 lg:mx-12 p-4 lg:p-6">
+        <a href="#">
           <img src={bookmark} alt="Bookmark" className="bookmark-icon" />
         </a>
       </div>
-      <div className="navbar-end lg:flex sm:flex md:flex">
-        <ul className="menu menu-horizontal uppercase gap-6 flex items-center">
+
+      <div className="flex-grow flex justify-end mx-8 lg:mx-12">
+        <ul className="menu menu-horizontal uppercase gap-8 flex items-center">
           {navigation.map((navi) => (
             <li key={navi}>
               <a className="hover:text-lightRed hover:bg-transparent">{navi}</a>
             </li>
           ))}
           <li>
-            <a className="bg-lightRed text-white px-8 py-3 hover:bg-white hover:text-lightRed hover:border-solid hover:border-4 hover:border-current">
+            <a className="bg-lightRed text-white px-9 py-3 shadow-md hover:bg-white hover:text-lightRed hover:border-solid hover:border-4 hover:border-current hover:shadow-lg">
               login
             </a>
           </li>
