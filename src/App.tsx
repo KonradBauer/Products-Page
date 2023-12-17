c3989205004a1f843519dd48427607883427a295
Konrad Bauer <konradbauer94@gmail.com>
2023-12-17 01:12:39 +0100

    remove template test div, import Header component, add parent div with global width, margin, font and padding top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { Header } from "./components/Header/Header";
+
 export const App = () => {
   return (
-    <div className="bg-blue-200">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fuga saepe qui dolores
-      exercitationem cum earum ullam sequi. Voluptatem deleniti unde consequatur debitis molestias
-      accusantium eius fugit excepturi dolorum reprehenderit.
+    <div className="max-w-6xl m-auto pt-8 font-rubik font-bold">
+      <Header />
     </div>
   );
 };
