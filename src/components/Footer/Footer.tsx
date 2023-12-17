99dd0008cfe021b2517d0e4ed9238d1d2d2d9bad
Konrad Bauer <konradbauer94@gmail.com>
2023-12-18 00:07:49 +0100

    footer now is responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,12 +6,10 @@ export const Footer: React.FC = () => {
   const navigation = ["features", "pricing", "contact"];
 
   return (
-    <div className="navbar flex flex-col lg:flex-row items-center justify-between tracking-wider bg-darkBlue p-6 lg:px-24">
-      <div className="mx-8 lg:mx-12 p-4 lg:p-6">
-        <a>
-          <img src={bookmark} alt="Bookmark" className="bookmark-icon flex-shrink" />
-        </a>
-        <ul className="menu menu-horizontal uppercase gap-6 flex items-center mt-4 lg:mt-0 lg:ml-10">
+    <div className="navbar flex flex-col lg:flex-row justify-between tracking-wider bg-darkBlue p-6 lg:px-[56px] sm:flex-wrap">
+      <img src={bookmark} alt="Bookmark" className="bookmark-icon text-white" />
+      <div className="mx-8 lg:mx-12 p-4 lg:p-6 flex flex-1 ">
+        <ul className="menu menu-horizontal uppercase gap-6 flex mt-4 lg:mt-0 lg:ml-10 max-md:flex-col">
           {navigation.map((navi) => (
             <li key={navi}>
               <a className="text-lightGrey hover:text-lightRed hover:bg-transparent">{navi}</a>
