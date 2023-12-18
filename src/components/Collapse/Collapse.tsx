42a0880a733d8500129978f12e067b45c8f5bc26
Konrad Bauer <konradbauer94@gmail.com>
2023-12-19 00:06:26 +0100

    created collapse component with props

---
+++ src/components/Collapse/Collapse.tsx
@@ -0,0 +1,20 @@
+interface CollapseProps {
+  title: string;
+  content: string;
+}
+
+export const Collapse: React.FC<CollapseProps> = ({ title, content }) => {
+  return (
+    <div className="w-[55%] mx-auto font-rubik text-left">
+      <br className="text-lightRed" />
+      <details className="collapse collapse-arrow bg-base-200 bg-transparent">
+        <summary className="collapse-title text-2xl font-bold text-left transition hover:text-lightRed border-t-4 max-md:border-t-1 max-md:text-sm">
+          {title}
+        </summary>
+        <div className="collapse-content text-2xl transition">
+          <p className="text-left mt-10 max-md:text-sm max-md:mt-2">{content}</p>
+        </div>
+      </details>
+    </div>
+  );
+};
