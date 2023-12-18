049b6881fc49407f7ceff3b68c5b4113f1b219ec
Konrad Bauer <konradbauer94@gmail.com>
2023-12-18 20:53:42 +0100

    added timer to decrement populationText, added types and email validation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import bookmark from "../../images/logo-bookmark-white.svg";
 import facebook from "../../images/icon-facebook.svg";
 import twitter from "../../images/icon-twitter.svg";
 import { Button } from "../Button/Button";
 
-export const Footer: React.FC = () => {
-  const navigation = ["features", "pricing", "contact"];
+interface FooterProps {}
 
-  const populationText = "35.000+ already joined";
-  const newsletterHeadText = "Stay up-to-date with what we're doing";
+interface NavigationItem {
+  key: string;
+}
 
-  const [email, setEmail] = useState("");
-  const [isValidEmail, setIsValidEmail] = useState(true);
+interface State {
+  populationText: string;
+  email: string;
+  isValidEmail: boolean;
+}
+
+const initialPopulation = 35000;
+const decrementPerSecond = initialPopulation / 20;
+
+export const Footer: React.FC<FooterProps> = () => {
+  const navigation: NavigationItem[] = [
+    { key: "features" },
+    { key: "pricing" },
+    { key: "contact" },
+  ];
+
+  const [state, setState] = useState<State>({
+    populationText: `${initialPopulation}+ already joined`,
+    email: "",
+    isValidEmail: true,
+  });
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    setIsValidEmail(validateEmail(e.target.value));
+    setState((prevState) => ({
+      ...prevState,
+      email: e.target.value,
+      isValidEmail: validateEmail(e.target.value),
+    }));
   };
 
   const validateEmail = (email: string) => {
@@ -24,29 +46,46 @@ export const Footer: React.FC = () => {
   };
 
   const handleButtonClick = () => {
-    if (isValidEmail) {
-      console.log("Email is valid:", email);
+    if (state.isValidEmail) {
+      console.log("Email is valid:", state.email);
     } else {
       console.log("Invalid email address");
     }
   };
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setState((prevState) => {
+        const newPopulation = Math.max(
+          0,
+          parseFloat(prevState.populationText) - decrementPerSecond
+        );
+        return {
+          ...prevState,
+          populationText: `${newPopulation.toFixed(0)} already joined`,
+        };
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
       <div className="bg-lightBlue text-white flex flex-col items-center p-4 max-sm: pb-0 flex-wrap font-rubik">
         <span className="text text-sm font-normal uppercase tracking-widest w-full flex justify-center mt-14">
-          {populationText}
+          {state.populationText}
         </span>
         <span className="text-3xl max-sm:text-2xl flex justify-center text-center mt-10 mb-6 max-sm:mt-2 mx-0 font-bold tracking-wide lg:max-w-md">
-          {newsletterHeadText}
+          Stay up-to-date with what we're doing
         </span>
         <div className="max-sm:flex-wrap mb-16 lg:w-2/4 flex justify-center items-center">
           <input
             className={`input input-bordered rounded-md join-item w-[40%] max-sm:w-[95%] text-black lg:mr-4 md:mr-4 max-sm:mb-4 ${
-              isValidEmail ? "" : "border-error"
+              state.isValidEmail ? "" : "border-error"
             }`}
             placeholder="Enter your email address"
-            value={email}
+            value={state.email}
             onChange={handleEmailChange}
           />
           <Button text="contact us" capitalize width onClick={handleButtonClick} />
@@ -58,8 +97,10 @@ export const Footer: React.FC = () => {
         <div className="mx-8 lg:mx-12 p-4 lg:p-6 flex flex-1">
           <ul className="menu menu-horizontal uppercase gap-6 flex mt-4 lg:mt-0 lg:ml-10 max-md:flex-col items-center">
             {navigation.map((navi) => (
-              <li key={navi}>
-                <a className="text-lightGrey hover:text-lightRed hover:bg-transparent">{navi}</a>
+              <li key={navi.key}>
+                <a className="text-lightGrey hover:text-lightRed hover:bg-transparent">
+                  {navi.key}
+                </a>
               </li>
             ))}
           </ul>
