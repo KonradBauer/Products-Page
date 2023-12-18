c9c40a11c0070695e3e46259eb0a80bc65df1f00
Konrad Bauer <konradbauer94@gmail.com>
2023-12-19 00:10:38 +0100

    mobile styled collapse

--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -5,10 +5,10 @@ interface CollapseProps {
 
 export const Collapse: React.FC<CollapseProps> = ({ title, content }) => {
   return (
-    <div className="w-[55%] mx-auto font-rubik text-left">
+    <div className="w-[55%] mx-auto max-md:mx-0 max-md:w-full font-rubik-500 text-left">
       <br className="text-lightRed" />
       <details className="collapse collapse-arrow bg-base-200 bg-transparent">
-        <summary className="collapse-title text-2xl font-bold text-left transition hover:text-lightRed border-t-4 max-md:border-t-1 max-md:text-sm">
+        <summary className="collapse-title text-2xl font-bold text-left transition hover:text-lightRed border-t-4 max-md:border max-md:text-sm">
           {title}
         </summary>
         <div className="collapse-content text-2xl transition">
