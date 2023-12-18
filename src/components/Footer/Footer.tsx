fe57115e95ee9fb9f6a588a45e21f2b277f2cc77
Konrad Bauer <konradbauer94@gmail.com>
2023-12-18 19:07:44 +0100

    create upper content on Footer, added email validation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,81 @@
+import React, { useState } from "react";
 import bookmark from "../../images/logo-bookmark-white.svg";
 import facebook from "../../images/icon-facebook.svg";
 import twitter from "../../images/icon-twitter.svg";
+import { Button } from "../Button/Button";
 
 export const Footer: React.FC = () => {
   const navigation = ["features", "pricing", "contact"];
 
+  const populationText = "35.000+ already joined";
+  const newsletterHeadText = "Stay up-to-date with what we're doing";
+
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setIsValidEmail(validateEmail(e.target.value));
+  };
+
+  const validateEmail = (email: string) => {
+    // Prosta walidacja emaila, możesz dostosować według swoich potrzeb
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleButtonClick = () => {
+    if (isValidEmail) {
+      // Obsługa logiki, gdy email jest poprawny
+      console.log("Email is valid:", email);
+    } else {
+      // Obsługa logiki, gdy email jest niepoprawny
+      console.log("Invalid email address");
+    }
+  };
+
   return (
-    <div className="navbar flex flex-col lg:flex-row justify-between tracking-wider bg-darkBlue p-6 lg:px-[56px] sm:flex-wrap">
-      <img src={bookmark} alt="Bookmark" className="bookmark-icon text-white" />
-      <div className="mx-8 lg:mx-12 p-4 lg:p-6 flex flex-1">
-        <ul className="menu menu-horizontal uppercase gap-6 flex mt-4 lg:mt-0 lg:ml-10 max-md:flex-col">
-          {navigation.map((navi) => (
-            <li key={navi}>
-              <a className="text-lightGrey hover:text-lightRed hover:bg-transparent">{navi}</a>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className="bg-lightBlue text-white flex flex-col items-center p-4 max-sm: pb-0 flex-wrap font-rubik">
+        <span className="text text-sm font-normal uppercase tracking-widest w-full flex justify-center mt-14">
+          {populationText}
+        </span>
+        <span className="text-3xl max-sm:text-2xl flex justify-center text-center mt-10 mb-6 max-sm:mt-2 mx-0 font-bold tracking-wide lg:max-w-md">
+          {newsletterHeadText}
+        </span>
+        <div className="max-sm:flex-wrap mb-16 lg:w-2/4 flex justify-center items-center">
+          <input
+            className={`input input-bordered rounded-md join-item w-[40%] max-sm:w-[95%] text-black lg:mr-4 md:mr-4 max-sm:mb-4 ${
+              isValidEmail ? "" : "border-error"
+            }`}
+            placeholder="Enter your email address"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <Button text="contact us" capitalize width onClick={handleButtonClick} />
+        </div>
       </div>
-      <div className="flex gap-10 mx-6">
-        <a className="facebook-icon cursor-pointer">
-          <img src={facebook} alt="Facebook" />
-        </a>
-        <a className="twitter-icon cursor-pointer">
-          <img src={twitter} alt="Twitter" />
-        </a>
+
+      <div className="navbar flex flex-col lg:flex-row justify-between tracking-wider bg-darkBlue p-6 lg:px-[56px] sm:flex-wrap font-bold">
+        <img src={bookmark} alt="Bookmark" className="bookmark-icon text-white" />
+        <div className="mx-8 lg:mx-12 p-4 lg:p-6 flex flex-1">
+          <ul className="menu menu-horizontal uppercase gap-6 flex mt-4 lg:mt-0 lg:ml-10 max-md:flex-col items-center">
+            {navigation.map((navi) => (
+              <li key={navi}>
+                <a className="text-lightGrey hover:text-lightRed hover:bg-transparent">{navi}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex gap-10 mx-6">
+          <a className="facebook-icon cursor-pointer">
+            <img src={facebook} alt="Facebook" />
+          </a>
+          <a className="twitter-icon cursor-pointer">
+            <img src={twitter} alt="Twitter" />
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
