2e42dbe7b9c090bc846bffbdd16dc9251eabd7cc
Konrad Bauer <konradbauer94@gmail.com>
2023-12-19 17:13:14 +0100

    Button new prop marginTop added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,21 @@ interface ButtonProps {
   capitalize?: boolean;
   width?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  marginTop: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, text, capitalize = false, width }) => {
+export const Button: React.FC<ButtonProps> = ({
+  onClick,
+  text,
+  capitalize = false,
+  width,
+  marginTop,
+}) => {
   return (
     <button
       className={`btn btn-error ${capitalize ? "capitalize" : "uppercase"}
       ${width ? "max-sm:w-[95%]" : ""}
+      ${marginTop ? "max-sm:mt-4" : ""}
       text-white px-10 pt-1 tracking-wider shadow-lg hover:bg-white hover:text-lightRed hover:`}
       onClick={onClick}
     >
