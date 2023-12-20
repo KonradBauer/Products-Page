fd062502bbaf9d20921c245d7a1409cc6f3a2cdb
Konrad Bauer <konradbauer94@gmail.com>
2023-12-20 21:05:52 +0100

    adjust subheader rwd

--- src/components/Subheader/Subheader.tsx
+++ src/components/Subheader/Subheader.tsx
@@ -7,14 +7,14 @@ export const Subheader = () => {
         <div className="hero-content flex-col lg:flex-row-reverse gap-12">
           <img src={hero} className="max-w-2xl rounded-lg w-full" />
           <div>
-            <h1 className="text-5xl font-bold max-w-lg max-md:text-[28px] max-md:flex max-md:text-center">
+            <h1 className="text-5xl font-bold max-w-lg max-md:text-[28px] max-md:flex max-lg:text-center max-md:justify-center">
               A Simple Bookmark Manager
             </h1>
-            <p className="py-6 text-lightGrey text-lg max-w-lg max-md:mx-3 max-md:text-center">
+            <p className="py-6 text-lightGrey text-2xl max-w-lg max-md:mx-4 max-md:text-center max-md:text-[16px]">
               A clean and simple interface to organize your favourite websites. Open a new browser
               tab and see you sites load instantly. Try it for free.
             </p>
-            <div className="mt-4 max-md:flex justify-center">
+            <div className="mt-4 max-lg:flex justify-center">
               <button className="btn bg-lightBlue text-white border-transparent border-2 font-bold hover:bg-white hover:text-lightBlue hover:border-lightBlue px-8 mr-6 max-md:mr-4 max-md:px-4">
                 Get it on Chrome
               </button>
