91dbf8106f86d4ae498669b57fe6336b0e2a514b
Konrad Bauer <konradbauer94@gmail.com>
2023-12-20 21:04:50 +0100

    added customClassName props to Collapse

--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -1,11 +1,16 @@
 interface CollapseProps {
   title: string;
   content: string;
+  customClassName?: string;
 }
 
-export const Collapse: React.FC<CollapseProps> = ({ title, content }) => {
+export const Collapse: React.FC<CollapseProps> = ({ title, content, customClassName }) => {
   return (
-    <div className="w-[55%] mx-auto max-md:mx-0 max-md:w-full font-rubik-500 text-left">
+    <div
+      className={`w-[55%] mx-auto max-md:mx-0 max-md:w-full font-rubik-500 text-left  ${
+        customClassName || ""
+      }`}
+    >
       <details className="collapse collapse-arrow">
         <summary className="collapse-title text-2xl font-bold text-left mb-8 transition hover:text-lightRed md:border-t-4 max-md:border-b-2 max-md:text-[16px]">
           {title}
