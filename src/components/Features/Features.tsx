5c41c5c221d9e20aa8b9becc49e3a4ad2e99048e
Konrad Bauer <konradbauer94@gmail.com>
2023-12-20 23:42:02 +0100

    create separated components templates for use it on Features

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,47 +1,57 @@
 import React, { useState } from "react";
+import { SimpleBookmarking } from "./SimpleBookmarking";
+import { SpeedySearching } from "./SpeedySearching";
+import { EasySharing } from "./EasySharing";
 
 interface Tab {
   label: string;
-  content: string;
+  content: React.ReactNode;
 }
 
 export const Features: React.FC = () => {
   const tabs: Tab[] = [
-    { label: "Simple Bookmarking", content: "Tab content 1" },
-    { label: "Speedy Searching", content: "Tab content 2" },
-    { label: "Easy Sharing", content: "Tab content 3" },
+    { label: "Simple Bookmarking", content: <SimpleBookmarking /> },
+    { label: "Speedy Searching", content: <SpeedySearching /> },
+    { label: "Easy Sharing", content: <EasySharing /> },
   ];
 
-  const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(tabs[0].label);
+
+  const redColor =
+    "text-lightRed border-lightRed hover:text-red-500 focus:border-b-1 border-red-500";
 
   return (
     <div className="text-center font-rubik-500 mt-24">
       <div className="text-[42px] font-bold max-md:text-[25px] max-md:mx-2">Features</div>
-      <div className="text-2xl text-lightGrey mx-96 mt-6 mb-24 max-md:mb-16 max-md:text-[16px] max-md:mx-8 max-lg:mx-24">
+      <div className="text-2xl text-lightGrey font-semibold mx-96 mt-6 mb-24 max-md:mb-16 max-md:text-[16px] max-md:mx-8 max-lg:mx-24">
         Our aim is to make it quick and easy for you to access your favourite websites. Your
         bookmarks sync between your devices so you can access them on the go.
       </div>
-      <div className="tabs tabs-bordered mx-72">
-        {tabs.map((tab, index) => (
-          <React.Fragment key={index}>
+      <div
+        className={
+          "tabs tabs-bordered mx-6 sm:mx-72 flex flex-col items-center sm:flex-row sm:justify-center"
+        }
+      >
+        {tabs.map((tab) => (
+          <React.Fragment key={tab.label}>
             <label
-              className={`tab text-xl pb-14 cursor-pointer font-bold focus:border-b-1 border-lightRed relative ${
-                index === selectedTab
-                  ? "text-black font-bold border-b-1 border-lightRed"
-                  : "text-lightGrey hover:text-lightRed"
+              className={`tab text-xl pb-14 cursor-pointer font-semibold border-red relative max-md:pt-4 ${
+                tab.label === selectedTab
+                  ? `text-black font-bold border-b-1 ${redColor}`
+                  : "text-lightGrey hover:text-lightRed border-lightRed"
               }`}
             >
               <input
                 type="radio"
-                name={`my_tabs_${index}`}
+                name={`my_tabs_${tab.label}`}
                 role="tab"
                 className="hidden"
                 aria-label={tab.label}
-                checked={index === selectedTab}
-                onChange={() => setSelectedTab(index)}
+                checked={tab.label === selectedTab}
+                onChange={() => setSelectedTab(tab.label)}
               />
-              {index === selectedTab && (
-                <div className="absolute inset-x-0 bottom-0 h-1 bg-lightRed"></div>
+              {tab.label === selectedTab && (
+                <div className="absolute inset-x-0 bottom-0 h-1 bg-lightRed md-max:p-4"></div>
               )}
               {tab.label}
             </label>
@@ -49,11 +59,11 @@ export const Features: React.FC = () => {
         ))}
       </div>
       <div className="mt-8">
-        {tabs.map((tab, index) => (
+        {tabs.map((tab) => (
           <div
-            key={index}
+            key={tab.label}
             role="tabpanel"
-            className={`tab-content p-10 ${index === selectedTab ? "" : "hidden"}`}
+            className={`tab-content p-10 ${tab.label === selectedTab ? "" : ""}`}
           >
             {tab.content}
           </div>
---
+++ src/components/Features/SpeedySearching.tsx
@@ -0,0 +1,30 @@
+import hero from "../../images/illustration-features-tab-2.svg";
+
+export const SpeedySearching = () => {
+  return (
+    <div className="flex mt-12 mb-10 max-md:mt-16">
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row-reverse gap-12">
+          <img src={hero} className="max-w-2xl rounded-lg w-full" />
+          <div>
+            <h1 className="text-5xl font-bold max-w-lg max-md:text-[28px] max-md:flex max-lg:text-center max-md:justify-center">
+              A Simple Bookmark Manager
+            </h1>
+            <p className="py-6 text-lightGrey font-semibold text-2xl max-w-lg max-md:mx-4 max-md:text-center max-md:text-[16px]">
+              A clean and simple interface to organize your favourite websites. Open a new browser
+              tab and see you sites load instantly. Try it for free.
+            </p>
+            <div className="mt-4 max-lg:flex justify-center">
+              <button className="btn bg-lightBlue text-white border-transparent border-2 font-bold hover:bg-white hover:text-lightBlue hover:border-lightBlue px-8 mr-6 max-md:mr-4 max-md:px-4">
+                Get it on Chrome
+              </button>
+              <button className="btn bg-[#f7f7f7] text-[#626471] font-bold border-transparent border-2 hover:border-[#626471] hover:bg-white px-8 max-md:px-4">
+                Get it on Firefox
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
