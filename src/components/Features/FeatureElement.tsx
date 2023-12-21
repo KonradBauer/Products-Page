1d19a31409dbae4293a735dd7ea12acf56b2c72e
Konrad Bauer <konradbauer94@gmail.com>
2023-12-21 16:38:57 +0100

    blue ornament implemented on mobiles

--- src/components/Features/FeatureElement.tsx
+++ src/components/Features/FeatureElement.tsx
@@ -8,7 +8,7 @@ export const FeatureElement: React.FC<FeatureElementProps> = ({ hero, title, des
   return (
     <div className="mt-12 mb-32 max-md:mt-4 flex justify-center">
       <div className="flex flex-col lg:flex-row gap-32 max-md:gap-2 items-start">
-        <div className="absolute bg-lightBlue h-96 mt-20 w-[39%] left-0 rounded-r-full"></div>
+        <div className="absolute bg-lightBlue h-96 mt-20 w-[39%] left-0 rounded-r-full max-md:w-[83%] max-md:h-48 max-md:mt-8" />
         <img src={hero} className="max-w-2xl rounded-lg w-full lg:w-auto relative" />
         <div className="text-left">
           <h1 className="text-4xl font-bold mt-12 max-md:text-2xl text-left max-md:text-center">
