7905aa64bac156d7ca161bfba90c0583f63660ab
Konrad Bauer <konradbauer94@gmail.com>
2023-12-21 03:54:00 +0100

    mobile nav implemented

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import bookmark from "../../images/logo-bookmark.svg";
+import navbookmark from "../../images/logo-bookmark-nav.svg";
 import burgerButton from "../../images/icon-hamburger.svg";
+import closeNav from "../../images/icon-close.svg";
 import { Button } from "../Button/Button";
+import { Facebook } from "../SvgComponents/FacebookIcon";
+import { Twitter } from "../SvgComponents/TwitterIcon";
 
 export const Header: React.FC = () => {
   const navigation = ["features", "pricing", "contact"];
@@ -11,28 +15,85 @@ export const Header: React.FC = () => {
     setIsMenuOpen((prev) => !prev);
   };
 
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) {
+      body.classList.toggle("overflow-hidden", isMenuOpen);
+    }
+
+    return () => {
+      if (body) {
+        body.classList.remove("overflow-hidden");
+      }
+    };
+  }, [isMenuOpen]);
+
   return (
-    <div className="flex items-center justify-between mx-20 lg:p-6 font-rubik-500 max-md:mx-8 max-md:mt-2">
-      <div>
-        <img src={bookmark} alt="Bookmark" className="bookmark-icon" />
+    <div className="relative">
+      {isMenuOpen && <div className="fixed inset-0 bg-darkBlue z-50" onClick={toggleMenu} />}
+      <div className="flex items-center justify-between mx-20 lg:p-6 font-rubik-500 max-md:mx-8 max-md:mt-2 relative z-50">
+        {isMenuOpen && (
+          <div className="flex flex-row flex-1">
+            <img src={navbookmark} alt="Bookmark" className="" />
+          </div>
+        )}
+        <div className="flex flex-row">
+          <img src={bookmark} alt="Bookmark" className={`${isMenuOpen ? "hidden" : ""}`} />
+        </div>
+        <div className="md:hidden">
+          <button className="burger-btn" onClick={toggleMenu} aria-label="Toggle menu">
+            {isMenuOpen ? (
+              <img src={closeNav} alt="CloseButton" className="burger-btn" />
+            ) : (
+              <img src={burgerButton} alt="BurgerButton" className="hamburger-icon" />
+            )}
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="md:hidden">
+            <ul
+              className={`menu uppercase flex-col items-center absolute left-0 w-full bg-darkBlue opacity-100 mt-12 ${
+                isMenuOpen ? "h-screen min-h-screen" : ""
+              }`}
+            >
+              {navigation.map((navi) => (
+                <li key={navi}>
+                  <a
+                    className={`hover:text-lightRed hover:bg-transparent font-bold p-4 ${
+                      isMenuOpen ? "text-white font-md rounded-none mt-4 font-normal text-xl" : ""
+                    }`}
+                    onClick={toggleMenu}
+                  >
+                    {navi}
+                  </a>
+                </li>
+              ))}
+              <button className="w-full border-2 py-2 rounded-md uppercase mt-4 text-xl bg-transparent text-white">
+                login
+              </button>
+              <div className="flex justify-center mt-40 gap-6">
+                <a className="cursor-pointer mx-2">
+                  <Facebook />
+                </a>
+                <a className="cursor-pointer mx-2">
+                  <Twitter />
+                </a>
+              </div>
+            </ul>
+          </div>
+        )}
+
+        <div className="hidden md:flex md:items-center md:space-x-6">
+          <ul className="menu menu-horizontal uppercase flex items-center">
+            {navigation.map((navi) => (
+              <li key={navi}>
+                <a className="hover:text-lightRed hover:bg-transparent font-bold">{navi}</a>
+              </li>
+            ))}
+          </ul>
+          <Button text="login" />
+        </div>
       </div>
-      <div className="md:hidden">
-        <button className="burger-btn" onClick={toggleMenu} aria-label="Toggle menu">
-          <img src={burgerButton} alt="BurgerButton" className="hamburger-icon" />
-        </button>
-      </div>
-      <ul
-        className={`menu menu-horizontal uppercase gap-6 flex items-center ${
-          isMenuOpen ? "" : "hidden md:flex"
-        }`}
-      >
-        {navigation.map((navi) => (
-          <li key={navi}>
-            <a className="hover:text-lightRed hover:bg-transparent font-bold">{navi}</a>
-          </li>
-        ))}
-        <Button text="login" />
-      </ul>
     </div>
   );
 };
