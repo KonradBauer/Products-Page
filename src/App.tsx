5b592e2d5133e2b42e7c47e9d30780117218c844
Konrad Bauer <konradbauer94@gmail.com>
2024-03-10 23:22:04 +0100

    fix dependency table in show modal useEffect after 30 seconds

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Footer } from "./components/Footer/Footer";
 import { GlobalContainer } from "./components/GlobalContainer/GlobalContainer";
 import { Header } from "./components/Header/Header";
 import { Subheader } from "./components/Subheader/Subheader";
-
 import { Modal } from "./components/Modal/Modal";
 
 export const App: React.FC = () => {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -45,68 +45,100 @@ export const Header: React.FC = () => {
     }
   };
 
-  setTimeout(() => {
-    openModal();
-  }, 30000);
+  useEffect(() => {
+    setTimeout(() => {
+      openModal();
+    }, 30000);
+  }, []);
 
   return (
-    <div className="relative">
-      {isMenuOpen && <div className="fixed inset-0 z-50 bg-darkBlue" />}
+    <>
+      <div onMouseEnter={openModal} style={{ height: "10px", width: "100%" }} />
+      <div className="relative">
+        {isMenuOpen && <div className="fixed inset-0 z-50 bg-darkBlue" />}
 
-      <div className="font-rubik-500 relative z-50 mx-20 flex items-center justify-between max-md:mx-8 max-md:mt-2 lg:p-6">
-        {isMenuOpen && (
-          <div className="flex flex-1 flex-row">
-            <img src={navbookmark} alt="Bookmark" />
+        <div className="font-rubik-500 relative z-50 mx-20 flex items-center justify-between max-md:mx-8 max-md:mt-2 lg:p-6">
+          {isMenuOpen && (
+            <div className="flex flex-1 flex-row">
+              <img src={navbookmark} alt="Bookmark" />
+            </div>
+          )}
+
+          <div className="flex flex-row">
+            <img
+              src={bookmark}
+              alt="Bookmark"
+              className={`${isMenuOpen ? "hidden" : ""}`}
+            />
           </div>
-        )}
 
-        <div className="flex flex-row">
-          <img
-            src={bookmark}
-            alt="Bookmark"
-            className={`${isMenuOpen ? "hidden" : ""}`}
-            onMouseEnter={openModal}
-          />
-        </div>
-
-        <div className="md:hidden">
-          <button
-            className="burger-btn"
-            onClick={toggleMenu}
-            aria-label="Toggle menu"
-          >
-            {isMenuOpen ? (
-              <img src={closeNav} alt="CloseButton" className="burger-btn" />
-            ) : (
-              <img
-                src={burgerButton}
-                alt="BurgerButton"
-                className="hamburger-icon"
-              />
-            )}
-          </button>
-        </div>
-
-        {isMenuOpen && (
           <div className="md:hidden">
-            <ul
-              className={`menu absolute left-0 mt-12 w-full flex-col items-center bg-darkBlue uppercase opacity-100 ${
-                isMenuOpen ? "h-screen min-h-screen" : ""
-              }`}
+            <button
+              className="burger-btn"
+              onClick={toggleMenu}
+              aria-label="Toggle menu"
             >
+              {isMenuOpen ? (
+                <img src={closeNav} alt="CloseButton" className="burger-btn" />
+              ) : (
+                <img
+                  src={burgerButton}
+                  alt="BurgerButton"
+                  className="hamburger-icon"
+                />
+              )}
+            </button>
+          </div>
+
+          {isMenuOpen && (
+            <div className="md:hidden">
+              <ul
+                className={`menu absolute left-0 mt-12 w-full flex-col items-center bg-darkBlue uppercase opacity-100 ${
+                  isMenuOpen ? "h-screen min-h-screen" : ""
+                }`}
+              >
+                {navigation.map((navi) => (
+                  <li
+                    key={navi}
+                    className="opa w-full items-center border-b tracking-widest"
+                  >
+                    <a
+                      className={`p-4 font-bold hover:bg-transparent hover:text-lightRed ${
+                        isMenuOpen
+                          ? "font-md mt-4 rounded-none text-xl font-normal text-white"
+                          : ""
+                      }`}
+                      onClick={() => {
+                        toggleMenu();
+                        scrollToSection(navi);
+                      }}
+                    >
+                      {navi}
+                    </a>
+                  </li>
+                ))}
+                <button className="mt-8 w-full rounded-md border-2 bg-transparent py-1 text-xl uppercase tracking-widest text-white">
+                  login
+                </button>
+                <div className="mt-40 flex justify-center gap-6">
+                  <a className="mx-2 cursor-pointer">
+                    <Facebook />
+                  </a>
+                  <a className="mx-2 cursor-pointer">
+                    <Twitter />
+                  </a>
+                </div>
+              </ul>
+            </div>
+          )}
+
+          <div className="hidden md:flex md:items-center md:space-x-6">
+            <ul className="menu menu-horizontal flex items-center uppercase">
               {navigation.map((navi) => (
-                <li
-                  key={navi}
-                  className="opa w-full items-center border-b tracking-widest"
-                >
+                <li key={navi}>
                   <a
-                    className={`p-4 font-bold hover:bg-transparent hover:text-lightRed ${
-                      isMenuOpen
-                        ? "font-md mt-4 rounded-none text-xl font-normal text-white"
-                        : ""
-                    }`}
+                    className="font-bold hover:bg-transparent hover:text-lightRed"
                     onClick={() => {
-                      toggleMenu();
                       scrollToSection(navi);
                     }}
                   >
@@ -114,39 +146,11 @@ export const Header: React.FC = () => {
                   </a>
                 </li>
               ))}
-              <button className="mt-8 w-full rounded-md border-2 bg-transparent py-1 text-xl uppercase tracking-widest text-white">
-                login
-              </button>
-              <div className="mt-40 flex justify-center gap-6">
-                <a className="mx-2 cursor-pointer">
-                  <Facebook />
-                </a>
-                <a className="mx-2 cursor-pointer">
-                  <Twitter />
-                </a>
-              </div>
             </ul>
+            <Button text="login" marginTop={false} specialWidth={false} />
           </div>
-        )}
-
-        <div className="hidden md:flex md:items-center md:space-x-6">
-          <ul className="menu menu-horizontal flex items-center uppercase">
-            {navigation.map((navi) => (
-              <li key={navi}>
-                <a
-                  className="font-bold hover:bg-transparent hover:text-lightRed"
-                  onClick={() => {
-                    scrollToSection(navi);
-                  }}
-                >
-                  {navi}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <Button text="login" />
         </div>
       </div>
-    </div>
+    </>
   );
 };
